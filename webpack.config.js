34bc140814ed7858cb82c76717dc864f4662a155
Lee <lee.gould@devgarden.se>
2020-05-14 14:04:43 +0200

    updates to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,76 +5,82 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 module.exports = {
-    entry: ["babel-polyfill", "./src/index.js"],
-    output: {
-        publicPath: "/",
-        path: path.resolve(__dirname, "dist")
-    },
-    //For showing correct line numbers and code in console
-    devtool: "cheap-module-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist")
+  },
+  //For showing correct line numbers and code in console
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
 
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-
-                        options: { minimize: true }
-                    },
-
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                // test: /\.less$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass'),
-                        },
-                    }
-
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-
-            {
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                loader: "file-loader"
-            }
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
         ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            favicon: "./src/favicon.ico"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new webpack.DefinePlugin({
-            BASEPATH: "''",
-            WEB_PACK_CONFIG_RUNNING: "'development-config'"
-        }),
-        new CopyWebpackPlugin([{ from: "src/img", to: "../dist/src/img" }])
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        // test: /\.less$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              reloadAll: true
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "file-loader"
+      }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new webpack.DefinePlugin({
+      BASEPATH: "''",
+      WEB_PACK_CONFIG_RUNNING: "'development-config'"
+    }),
+    new CopyWebpackPlugin([{ from: "src/img", to: "../dist/src/img" }])
+  ]
 };
